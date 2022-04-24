b69848dc600474a6db5a15019496c1f072d8928e
Yewon Lim <ga06033@yonsei.ac.kr>
2022-04-24 12:11:40 +0000

    teethimagetranslation update | 220424

---
+++ models/pix2pixTeethImg_model.py
@@ -0,0 +1,33 @@
+from .pix2pix_model import Pix2PixModel
+
+# Pix2Pix Model for Teeth to Teeth Image Translation 
+class Pix2PixTeethImgModel(Pix2PixModel):
+    def __init__(self, opt):
+        """Initialize the pix2pix class.
+
+        Parameters:
+            opt (Option class)-- stores all the experiment flags; needs to be a subclass of BaseOptions
+        """
+        Pix2PixModel.__init__(self, opt)
+    
+    @staticmethod
+    def modify_commandline_options(parser, is_train=True):
+        """Add new dataset-specific options, and rewrite default values for existing options.
+
+        Parameters:
+            parser          -- original option parser
+            is_train (bool) -- whether training phase or test phase. You can use this flag to add training-specific or test-specific options.
+
+        Returns:
+            the modified parser.
+
+        For pix2pix, we do not use image buffer
+        The training objective is: GAN Loss + lambda_L1 * ||G(A)-B||_1
+        By default, we use vanilla GAN loss, UNet with batchnorm, and aligned datasets.
+        """
+        # changing the default values to match the pix2pix paper (https://phillipi.github.io/pix2pix/)
+        parser.set_defaults(norm='batch', netG='unet_256', dataset_mode='aligned', direction='AtoB')
+        if is_train:
+            parser.set_defaults(pool_size=0, gan_mode='vanilla')
+            parser.add_argument('--lambda_L1', type=float, default=100.0, help='weight for L1 loss')
+        return parser
