3649a49dbadb914d3d2346290c4ade9a3e4b5db1
Yewon Lim <ga06033@yonsei.ac.kr>
2022-04-25 08:19:50 +0000

    fid metric added|220425

--- train.py
+++ train.py
@@ -23,10 +23,81 @@ from options.train_options import TrainOptions
 from data import create_dataset
 from models import create_model
 from util.visualizer import Visualizer
+import copy
+import numpy as np
+import torch
+from tqdm import tqdm
+import torchvision.transforms as TF
+from torchvision.models.inception import inception_v3
+from scipy import linalg
+from torch.nn.functional import adaptive_avg_pool2d
+
+# Pretrained inception model
+print(f"Loading pretrained inception v3 model...")
+inception = inception_v3(pretrained=True)
+inception = torch.nn.Sequential((inception.children())[:-1])
+
+
+def calculate_frechet_distance(mu1, mu2, cov1, cov2, eps=1e-6):
+    """Numpy implementation of the Frechet Distance.
+    The Frechet distance between two multivariate Gaussians X_1 ~ N(mu_1, C_1)
+    and X_2 ~ N(mu_2, C_2) is
+            d^2 = ||mu_1 - mu_2||^2 + Tr(C_1 + C_2 - 2*sqrt(C_1*C_2)).
+    Stable version by Dougal J. Sutherland.
+    Params:
+    -- mu1   : Numpy array containing the activations of a layer of the
+               inception net (like returned by the function 'get_predictions')
+               for generated samples.
+    -- mu2   : The sample mean over activations, precalculated on an
+               representative data set.
+    -- sigma1: The covariance matrix over activations for generated samples.
+    -- sigma2: The covariance matrix over activations, precalculated on an
+               representative data set.
+    Returns:
+    --   : The Frechet Distance.
+    """
+    
+    mu1 = np.atleast_1d(mu1)
+    mu2 = np.atleast_1d(mu2)
+
+    sigma1 = np.atleast_2d(cov1)
+    sigma2 = np.atleast_2d(cov2)
+
+    assert mu1.shape == mu2.shape, \
+        'Training and test mean vectors have different lengths'
+    assert sigma1.shape == sigma2.shape, \
+        'Training and test covariances have different dimensions'
+
+    diff = mu1 - mu2
+
+    # Product might be almost singular
+    covmean, _ = linalg.sqrtm(sigma1.dot(sigma2), disp=False)
+    if not np.isfinite(covmean).all():
+        msg = ('fid calculation produces singular product; '
+               'adding %s to diagonal of cov estimates') % eps
+        print(msg)
+        offset = np.eye(sigma1.shape[0]) * eps
+        covmean = linalg.sqrtm((sigma1 + offset).dot(sigma2 + offset))
+
+    # Numerical error might give slight imaginary component
+    if np.iscomplexobj(covmean):
+        if not np.allclose(np.diagonal(covmean).imag, 0, atol=1e-3):
+            m = np.max(np.abs(covmean.imag))
+            raise ValueError('Imaginary component {}'.format(m))
+        covmean = covmean.real
+
+    tr_covmean = np.trace(covmean)
+
+    return (diff.dot(diff) + np.trace(sigma1)
+            + np.trace(sigma2) - 2 * tr_covmean)
 
 if __name__ == '__main__':
     opt = TrainOptions().parse()   # get training options
     dataset = create_dataset(opt)  # create a dataset given opt.dataset_mode and other options
+    opt_val = copy(opt)
+    opt_val['phase']='val'
+
+    valid_data = create_dataset(opt_val)
     dataset_size = len(dataset)    # get the number of images in the dataset.
     print('The number of training images = %d' % dataset_size)
 
@@ -73,5 +144,25 @@ if __name__ == '__main__':
             print('saving the model at the end of epoch %d, iters %d' % (epoch, total_iters))
             model.save_networks('latest')
             model.save_networks(epoch)
+        
+        # calculate FID at every 10 epoch
+        if epoch % 10 == 1:
+            pred_features, true_features = [], []
+            print("Calculating FID...")
+            for i, val_data in tqdm(enumerate(valid_data)):
+                model.set_input(val_data)
+                model.forward()
+                # feature extraction
+                pred_feature = inception(model.fake_B)
+                true_feature = inception(val_data)
+                pred_features.append(pred_feature)
+                true_features.append(true_features)
+
+            # estimate the distribution - assuming the gaussian distribution
+            pred_features, true_features = torch.cat(pred_features), torch.cat(true_features)
+            mu_pred, cov_pred = pred_features.mean(axis=0), torch.cov(pred_features)
+            mu_true, cov_true = true_features.mean(axis=0), torch.cov(true_features)
+            fid = calculate_frechet_distance(mu_pred, mu_true, cov_pred, cov_true)
+            print(f"\tvalidation FID: {fid:5f}")
 
         print('End of epoch %d / %d \t Time Taken: %d sec' % (epoch, opt.n_epochs + opt.n_epochs_decay, time.time() - epoch_start_time))
